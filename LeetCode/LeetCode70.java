021e22f8bdd0fce8c186e813c6119a77ea6c3c39
Yu Feng <yu.feng@us.abb.com>
2021-11-23 20:36:17 -0600

    Java solution for LeetCode 70

---
+++ LeetCode/LeetCode70.java
@@ -0,0 +1,24 @@
+/**
+ * Question:
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * 
+ * Constraints: 1 <= n <= 45
+ */
+public class LeetCode70 {
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("n = 2, result = " + climbStairs(2));
+        System.out.println("n = 3, result = " + climbStairs(3));
+        System.out.println("n = 4, result = " + climbStairs(4));
+    }
+}
